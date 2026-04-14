b43c8caf50c97e8bc317863bfe8582636a979639
rafafrd <bolekao03@gmail.com>
2026-04-14 08:47:34 -0300

    feat: add buttons and fix img bug

--- js/server.js
+++ js/server.js
@@ -4,8 +4,9 @@ const pokemonNumber = document.querySelector(".pokemon__number");
 const pokemonName = document.querySelector(".pokemon__name");
 const form = document.getElementById("form");
 const input = document.querySelector("input");
-const btnPrev = document.getElementById("btn-prev");
-const btnNext = document.getElementById("btn-next");
+const btnPrev = document.querySelector(".btn__prev");
+const btnNext = document.querySelector(".btn__next");
+
 let searchPokemon = 150;
 // fetch -> interface Nativa do js, que faz as req http
 // .then() -> método de tratamento de resposta, promessa de resposta, quando a resposta chegar, o que fazer
@@ -21,7 +22,7 @@ const fetchData = async (pokemon) => {
     console.error("Erro ao buscar dados da API:", error);
   }
 };
-
+// até 649 tem animação, depois disso não tem mais, então tem que tratar isso
 const renderPokemon = async (pokemon) => {
   const data = await fetchData(pokemon);
   if (data) {
@@ -29,9 +30,13 @@ const renderPokemon = async (pokemon) => {
       data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
         "front_default"
       ];
-
+    if (searchPokemon > 649) {
+      pokemonImage.src = data["sprites"]["front_default"];
+    }
     pokemonNumber.innerText = `${data.id} -`;
     pokemonName.innerText = `${data.name}`;
+    searchPokemon = data.id;
+    input.value = "";
   }
 };
 
@@ -41,9 +46,20 @@ form.addEventListener("submit", (event) => {
   const pokemon = input.value.toLowerCase();
   renderPokemon(pokemon);
 });
-
-btnPrev.addEventListener("click", (event) => {});
-
-btnNext.addEventListener("click", (event) => {});
-
 renderPokemon(searchPokemon);
+
+btnPrev.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (searchPokemon > 1) {
+    searchPokemon--;
+    renderPokemon(searchPokemon);
+  }
+});
+
+btnNext.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (searchPokemon < 898) {
+    searchPokemon++;
+    renderPokemon(searchPokemon);
+  }
+});
